a73013c6b300376c53e6bd83f74168a938474dee
milo_meng <milo_meng@163.com>
2018-04-16 16:59:01 +0800

    修改Tab

--- lib/main.dart
+++ lib/main.dart
@@ -3,33 +3,54 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(home: MainTabs());
+  }
+}
 
-  final navigationBar = new BottomNavigationBar(
-    items: [
-      new BottomNavigationBarItem(
-          title: new Text('Bus'), icon: new Icon(Icons.directions_bus)),
-      new BottomNavigationBarItem(
-          title: new Text('Car'), icon: new Icon(Icons.directions_car)),
-      new BottomNavigationBarItem(
-          title: new Text('Subway'), icon: new Icon(Icons.directions_subway)),
-    ],
-  );
+class MainTabs extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => new MainTabsState();
+}
+
+class MainTabsState extends State<MainTabs>
+    with SingleTickerProviderStateMixin {
+  TabController controller;
+
+  @override
+  void initState() {
+    super.initState();
+    controller = TabController(vsync: this, length: 3);
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return new MaterialApp(
-      home: new DefaultTabController(
-        length: 3,
-        child: new Scaffold(
-          bottomNavigationBar: navigationBar,
-          body: new TabBarView(
-            children: <Widget>[
-              new Icon(Icons.directions_bus),
-              new Icon(Icons.directions_bike),
-              new Icon(Icons.directions_car),
-            ],
-          ),
+    return new Scaffold(
+      bottomNavigationBar: new Material(
+        color: Colors.deepOrange,
+        child: new TabBar(
+          controller: controller,
+          tabs: <Tab>[
+            new Tab(text: 'Bus', icon: new Icon(Icons.directions_bus)),
+            new Tab(text: 'Car', icon: new Icon(Icons.directions_car)),
+            new Tab(text: 'Subway', icon: new Icon(Icons.directions_subway)),
+          ],
         ),
+      ),
+      body: new TabBarView(
+        controller: controller,
+        children: <Widget>[
+          new Icon(Icons.directions_bus),
+          new Icon(Icons.directions_car),
+          new Icon(Icons.directions_subway),
+        ],
       ),
     );
   }
