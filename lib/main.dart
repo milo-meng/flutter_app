ff22b7040a273c6d5010374cf9f6c99b715fda00
milo_meng <milo_meng@163.com>
2018-04-16 09:50:00 +0800

    tab

--- lib/main.dart
+++ lib/main.dart
@@ -3,16 +3,32 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+
+  final navigationBar = new BottomNavigationBar(
+    items: [
+      new BottomNavigationBarItem(
+          title: new Text('Bus'), icon: new Icon(Icons.directions_bus)),
+      new BottomNavigationBarItem(
+          title: new Text('Car'), icon: new Icon(Icons.directions_car)),
+      new BottomNavigationBarItem(
+          title: new Text('Subway'), icon: new Icon(Icons.directions_subway)),
+    ],
+  );
+
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
-      title: 'FLutterApp',
-      home: new Scaffold(
-        appBar: new AppBar(
-          title: new Text('App'),
-        ),
-        body: new Center(
-          child: new Text('Hello World'),
+      home: new DefaultTabController(
+        length: 3,
+        child: new Scaffold(
+          bottomNavigationBar: navigationBar,
+          body: new TabBarView(
+            children: <Widget>[
+              new Icon(Icons.directions_bus),
+              new Icon(Icons.directions_bike),
+              new Icon(Icons.directions_car),
+            ],
+          ),
         ),
       ),
     );
